6af97d69d61e9ee00d0d467ff123ae4b4faf1b56
suabicii <michal.slabik3@wp.pl>
2022-10-03 19:08:05 +0200

    Created initial version of login form submit handler

--- screens/Login.js
+++ screens/Login.js
@@ -3,14 +3,35 @@ import {View} from "react-native";
 import {Text} from "react-native";
 import UserInput from "../components/auth/UserInput";
 import SubmitButton from "../components/auth/SubmitButton";
+import {API_URL} from "@env";
 
 function Login() {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [loading, setLoading] = useState(false);
 
-    const handleSubmit = () => {
-        console.log('Button was pressed');
+    const handleSubmit = async () => {
+        setLoading(true);
+        await fetch(`${API_URL}/api/login`, {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({email, password})
+        })
+            .then(data => data.json())
+            .then(data => {
+                if (data.error || data.status !== 200) {
+                    console.log('Not Logged In', data);
+                } else {
+                    console.log('Logged In', data);
+                }
+            })
+            .catch(err => {
+                console.log(err);
+            });
+        setLoading(false);
     }
 
     return (
