8186fd2c158c511244edee4d0b69fb4b7036ab00
suabicii <michal.slabik3@wp.pl>
2022-12-01 13:34:26 +0100

    Fixed bug in generating token function

--- utils/tokenGenerator.js
+++ utils/tokenGenerator.js
@@ -1,6 +1,6 @@
 export const generateToken = () => {
     let result = '';
-    const charPool = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^*?';
+    const charPool = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!#$^*';
     for (let i = 0; i < 16; i++) {
         result += charPool.charAt(Math.floor(Math.random() * charPool.length));
     }
