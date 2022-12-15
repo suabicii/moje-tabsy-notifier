a3268f779d2fa077edc42d44c9f9cf13aa80af13
suabicii <michal.slabik3@wp.pl>
2022-12-15 18:48:14 +0100

    Added removing dosing moment from list after pressing button to confirm the dose

--- components/content/Drugs.js
+++ components/content/Drugs.js
@@ -1,9 +1,11 @@
-import React from "react";
+import React, {useState} from "react";
 import {Button, Paragraph, Title} from "react-native-paper";
 import {View} from "react-native";
 
 function Drugs({drugList}) {
-    const dummyFn = () => {
+
+    const handleConfirmDose = () => {
+
     };
 
     return (
@@ -11,39 +13,45 @@ function Drugs({drugList}) {
             {
                 drugList.map(({dosing, name, unit, dosingMoments}, index) => {
                     const DosingMoments = () => {
+                        const [dosingMomentsToShow, setDosingMomentsToShow] = useState(Object.entries(dosingMoments));
                         const result = [];
-                        for (const key in dosingMoments) {
-                            if (dosingMoments.hasOwnProperty(key)) {
-                                result.push(
-                                    <View
+                        for (const [key, value] of dosingMomentsToShow) {
+                            const viewKey = `${name}${key}`;
+                            result.push(
+                                <View
+                                    style={{
+                                        alignItems: "center",
+                                        flexDirection: "row",
+                                        justifyContent: "center",
+                                        marginBottom: 10
+                                    }}
+                                    key={viewKey}
+                                >
+                                    <Paragraph style={{textAlign: "center", fontSize: 16}}>
+                                        – {`${value}`}
+                                    </Paragraph>
+                                    <Button
+                                        testID={viewKey}
                                         style={{
-                                            alignItems: "center",
-                                            flexDirection: "row",
-                                            justifyContent: "center",
-                                            marginBottom: 10
+                                            backgroundColor: "#6cc3d5",
+                                            color: "#e8e8e8",
+                                            marginLeft: 5
                                         }}
-                                        key={`${name}${key}`}
+                                        contentStyle={{flexDirection: "row-reverse"}}
+                                        icon="check"
+                                        mode="contained"
+                                        compact={true}
+                                        onPress={() => {
+                                            handleConfirmDose(viewKey);
+                                            setDosingMomentsToShow(
+                                                dosingMomentsToShow.filter(dosingMoment => dosingMoment[0] !== key)
+                                            );
+                                        }}
                                     >
-                                        <Paragraph style={{textAlign: "center", fontSize: 16}}>
-                                            - {dosingMoments[key]}
-                                        </Paragraph>
-                                        <Button
-                                            style={{
-                                                backgroundColor: "#6cc3d5",
-                                                color: "#e8e8e8",
-                                                marginLeft: 5
-                                            }}
-                                            contentStyle={{flexDirection: "row-reverse"}}
-                                            icon="check"
-                                            mode="contained"
-                                            compact={true}
-                                            onPress={() => dummyFn}
-                                        >
-                                            Już zażyłem/-am
-                                        </Button>
-                                    </View>
-                                );
-                            }
+                                        Już zażyłem/-am
+                                    </Button>
+                                </View>
+                            );
                         }
                         return result;
                     };
