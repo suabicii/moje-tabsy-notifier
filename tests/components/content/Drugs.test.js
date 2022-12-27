78bfd3eacd0b4e265a6a6ad9e87f74688df9c5c2
suabicii <michal.slabik3@wp.pl>
2022-12-27 18:43:04 +0100

    Fixed Drugs component test case

--- tests/components/content/Drugs.test.js
+++ tests/components/content/Drugs.test.js
@@ -2,8 +2,23 @@ import React from "react";
 import renderer from "react-test-renderer";
 import Drugs from "../../../components/content/Drugs";
 import {drugList} from "../../fixtures/drugList";
+import {DrugTakenContext} from "../../../context/DrugTakenContext";
+
+const drugTakenChecker = [];
+const setDrugTakenChecker = jest.fn();
+const mockUseContext = jest.fn().mockImplementation(() => ({drugTakenChecker, setDrugTakenChecker}));
+
+React.useContext = mockUseContext;
+
+function WrappedComponent() {
+    return (
+        <DrugTakenContext.Provider value={{drugTakenChecker, setDrugTakenChecker}}>
+            <Drugs drugList={drugList}/>
+        </DrugTakenContext.Provider>
+    );
+}
 
 it('should correctly render drug data', () => {
-    const tree = renderer.create(<Drugs drugList={drugList}/>).toJSON();
+    const tree = renderer.create(<WrappedComponent/>).toJSON();
     expect(tree).toMatchSnapshot();
 });
