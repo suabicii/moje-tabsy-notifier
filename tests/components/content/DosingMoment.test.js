5094719db1883eedad3ab30a49e9578be9fb6fec
suabicii <michal.slabik3@wp.pl>
2023-03-17 14:55:24 +0100

    Added missing tests of DosingMoment component for handleConfirmDose function

--- tests/components/content/DosingMoment.test.js
+++ tests/components/content/DosingMoment.test.js
@@ -7,6 +7,7 @@ import dayjs from "dayjs";
 import {Provider} from "react-redux";
 import store from "../../../store";
 import {setCurrentTime} from "../../../features/time/timeSlice";
+import {act, fireEvent, render, screen} from "@testing-library/react-native";
 
 beforeAll(() => {
     MockDate.set('2020-01-01');
@@ -14,8 +15,13 @@ beforeAll(() => {
     store.dispatch(setCurrentTime(JSON.stringify(mockedCurrentTime)));
 });
 
+let handleSetDosingMomentsToShow;
+
+beforeEach(() => {
+    handleSetDosingMomentsToShow = jest.fn();
+});
+
 const drug = drugList[0];
-const handleSetDosingMomentsToShow = jest.fn();
 const dosingMomentId = `${drug.name}_${Object.keys(drug.dosingMoments)[0]}`;
 
 function WrappedComponent() {
@@ -36,4 +42,53 @@ function WrappedComponent() {
 it('should correctly render single DosingMoment component', () => {
     const tree = renderer.create(<WrappedComponent/>).toJSON();
     expect(tree).toMatchSnapshot();
+});
+
+describe('handleConfirmDose', () => {
+    it('should handle setDosingMomentsToShow after pressing confirmation button', async () => {
+        jest.spyOn(global, 'fetch').mockImplementation(() => Promise.resolve({
+            json: () => Promise.resolve({
+                status: 200
+            })
+        }));
+        render(<WrappedComponent/>);
+
+        await act(() => {
+            fireEvent.press(screen.getByTestId(dosingMomentId));
+        });
+
+        expect(handleSetDosingMomentsToShow).toBeCalled();
+    });
+
+    it('should not handle setDosingMomentsToShow if request status does not equal 200', async () => {
+        jest.spyOn(global, 'fetch').mockImplementation(() => Promise.resolve({
+            json: () => Promise.resolve({
+                status: 404,
+                message: 'Drug id was not found'
+            })
+        }));
+        render(<WrappedComponent/>);
+
+        await act(() => {
+            fireEvent.press(screen.getByTestId(dosingMomentId));
+        });
+
+        expect(handleSetDosingMomentsToShow).not.toBeCalled();
+    });
+
+    it('should get error after pressing confirmation button if request was rejected', async () => {
+        jest.spyOn(global, 'fetch').mockImplementation(() => Promise.resolve({
+            json: () => Promise.reject({
+                status: 500,
+                message: 'Something went wrong'
+            })
+        }));
+        render(<WrappedComponent/>);
+
+        await act(() => {
+            fireEvent.press(screen.getByTestId(dosingMomentId));
+        });
+
+        expect(handleSetDosingMomentsToShow).not.toBeCalled();
+    });
 });
