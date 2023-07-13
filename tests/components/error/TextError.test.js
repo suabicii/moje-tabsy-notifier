282c2bcfc77b9202bdae4c56d94dc526ac189b00
suabicii <michal.slabik3@wp.pl>
2023-07-13 14:27:09 +0200

    Removing ignored files

--- tests/components/error/TextError.test.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import renderer from "react-test-renderer";
-import TextError from "../../../components/error/TextError";
-
-it('should correctly render TextError component with some content', () => {
-    const tree = renderer.create(<TextError content="Dummy error"/>).toJSON();
-    expect(tree).toMatchSnapshot();
-});
--- tests/utils/tokenGenerator.test.js
+++
@@ -1,7 +0,0 @@
-import {generateToken} from "../../utils/tokenGenerator";
-
-it('should properly generate token with 6 random characters', () => {
-    const token = generateToken();
-
-    expect(token.length).toBe(16);
-});
